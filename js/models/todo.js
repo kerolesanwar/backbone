c972ce22a50a8c2e43d71fa53e0f401e1c660e88
Keroles Anwar <keroles.anwar@gmail.com>
2014-03-08 18:14:26 -0500

    Added default date value to current date

--- js/models/todo.js
+++ js/models/todo.js
@@ -6,14 +6,15 @@ var app = app || {};
 
 	// Todo Model
 	// ----------
-
+	var currentDate = new Date();
 	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
 	app.Todo = Backbone.Model.extend({
 		// Default attributes for the todo
 		// and ensure that each todo created has `title` and `completed` keys.
 		defaults: {
 			title: '',
-			completed: false
+			completed: false,
+			date: currentDate.getMonth()+1 + '/' + currentDate.getDate() + '/' + currentDate.getFullYear()
 		},
 
 		// Toggle the `completed` state of this todo item.
