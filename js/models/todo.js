ad15810859e2b0f5629040724dca77452bf6b61e
Keroles Anwar <keroles.anwar@gmail.com>
2014-03-10 14:42:59 -0400

    Changed Date to setDate to prevent js syntax conflict

--- js/models/todo.js
+++ js/models/todo.js
@@ -14,7 +14,7 @@ var app = app || {};
 		defaults: {
 			title: '',
 			completed: false,
-			date: currentDate.getMonth()+1 + '/' + currentDate.getDate() + '/' + currentDate.getFullYear()
+			setDate: currentDate.getMonth()+1 + '/' + currentDate.getDate() + '/' + currentDate.getFullYear()
 		},
 
 		// Toggle the `completed` state of this todo item.
