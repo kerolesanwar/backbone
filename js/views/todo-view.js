b4e364f5e62436a0311f5824dd7c19cbb3f338cb
Keroles Anwar <keroles.anwar@gmail.com>
2014-03-13 16:00:40 -0400

    Changed swipe label to drag label, and the resulting function from clear to toggleCompleted

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -19,7 +19,8 @@ var app = app || {};
 		events: {
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
-			'swipe label': 'clear',
+			//'swipe label': 'clear',
+			'drag  label': 'toggleCompleted',
 			'click .destroy': 'clear',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
